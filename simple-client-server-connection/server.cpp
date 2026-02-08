e76b8d44406d9f870b9883117106e8bbaae62d69
Prashant Kumar <prashantkumar1251@gmail.com>
2026-02-08 17:18:50 +0530

    implemented a key-value redis server

--- simple-client-server-connection/server.cpp
+++ simple-client-server-connection/server.cpp
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<iostream>
+#include<arpa/inet.h>
 
 using namespace std;
 
 int main() {
-    
+    int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in addr = {0};
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
 }
